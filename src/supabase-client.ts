8f09a6072b7c58da724b658c9c34a042a6ff82e0
Anarasty <murloc.ninja16@gmail.com>
2026-06-17 19:14:58 +0300

    first commit

---
+++ src/supabase-client.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://omebrmnvtvgbajsgpgla.supabase.co",
+  "sb_publishable_CzjNRwoRJ5nvugs83RhoKA_T0e-HTq_"
+);
